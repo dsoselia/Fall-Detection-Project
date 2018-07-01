3a9ac96870bb2bce50e8748a49b5770b2fe68260
daviti soselia <davitisoselia@MacBook-Pro-2.attlocal.net>
2018-07-01 05:13:28 -0700

    update/ first model creewated

---
+++ main.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  1 00:41:01 2018
+
+@author: davitisoselia
+"""
+
+with open('merged.csv') as csv:
+    content = csv.readlines()
+for i in range(len(content)):
+    content[i] = content[i].split(',')
+
+
+#content = content[::10]
+
+'''
+
+    if (len(content[i][-1]) > 2):
+        print(i)
+   ''' 
+import numpy as np
+def generateNumpy(point, length = 50):
+    segment = []
+    falls = 0;
+    fell = [[0,1]]
+    for i in range(point, point + length):
+        if ('all' in content[i][-1]):
+            falls+=1
+        segment.append(content[i][3:-2])
+    if (falls == 1):
+        return
+    elif(falls>1):
+        fell = [[0,1]]
+    for i in range(len(segment)):
+        for j in range(len(segment[i])):
+            segment[i][j] = float(segment[i][j])
+    segment = np.array(segment)
+    return segment, fell
+
+ml,mk = generateNumpy(5)
+
+
+
+
+
+
+
+
+
+sensorNum = ml.shape[0]
+
+
+
+print(len((content[35232])))
+print(len(content[0]))
+
+
+
+
+
+
+
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Conv1D
+import numpy as np
+
+
+model = Sequential()
+model.add(LSTM(25, return_sequences=True, stateful=True, input_shape=(None, sensorNum),
+         batch_input_shape=(1, None, sensorNum)))
+model.add(LSTM(20, recurrent_dropout=0.2))
+#model.add(Dense(30, activation='relu'))
+#model.add(Dense(10, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+
+
+
+
+import random
+j = 0
+iter = 0
+modeln='abcdeg_l.h5'
+while(iter<5):
+    #avred = not avred
+    np_arr, y = generateNumpy(j)
+    print(j)
+    print(np_arr.shape)
+    x_train = np.transpose(np_arr).reshape(1,np_arr.shape[0],np_arr.shape[1])
+    x_train = x_train / 50
+    y_train = np.array(y)
+    model.fit(x_train, y_train, batch_size=1, nb_epoch=1, shuffle=False, verbose=1)
+    #print(j)
+    j=random.randint(1, len(content)-50)
+    j=random.randint(1, 5)
+    #j=random.randint(1264, 1896)
+    if(iter % 1000 == 0):
+        model.save(modeln)
+        print(iter)
+    iter+=1;
+    
