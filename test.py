23d273680f9fb90ee79544fc0a7e5b718dbdbb85
Davit Soselia <dsoselia@sdsu.edu>
2018-07-06 22:53:21 +0400

    added print during training

--- test.py
+++ test.py
@@ -101,8 +101,28 @@ def checkresult(point = random.randint(1, len(content)-50), length = random.rand
     #x_train = temp_storage / 50
     y_train = np.array(y)
     prediction = model.predict(x_train)
+    #print(y_train)
+    #print(prediction)
+    return (np.argmax(y_train)==np.argmax(prediction))
+
+confusion_matrix = [[0,0],[0,0]]
+def checkresult_confusion(point = random.randint(1, len(content)-50), length = random.randint(300, 1500), check_fall = False):
+    np_arr, y = get_fall() if check_fall else generate_numpy(point, length)
+    np_arr = np_arr / temp_storage
+    x_train = np.transpose(np_arr).reshape(1,np_arr.shape[0],np_arr.shape[1])
+    #x_train = temp_storage / 50
+    y_train = np.array(y)
+    prediction = model.predict(x_train)
     print(y_train)
     print(prediction)
+    if (np.argmax(y_train)==np.argmax(prediction) and np.argmax(y_train) == 0):
+        confusion_matrix[0][0] += 1
+    elif (np.argmax(y_train)==np.argmax(prediction) and np.argmax(y_train) == 1):
+        confusion_matrix[1][1] += 1
+    elif (np.argmax(y_train)!=np.argmax(prediction) and np.argmax(y_train) == 1):
+        confusion_matrix[1][0] += 1
+    elif (np.argmax(y_train)!=np.argmax(prediction) and np.argmax(y_train) == 0):
+        confusion_matrix[0][1] += 1
     return (np.argmax(y_train)==np.argmax(prediction))
 
 '''
@@ -114,16 +134,18 @@ def checkresult(point = random.randint(1, len(content)-50), length = random.rand
     print(y_train)
     return (np.argmax(y_train)==np.argmax(model.predict(x_train)))
 '''
-fall = True    
-correct = 0
-i = 0
-while i < 1000:
-    try:
-        fall = not fall
-        correct += (checkresult(check_fall = fall))
-        i+=1
-    except:
-        print(sys.exc_info()[0])
-
-print(correct)
+def test():
+    fall = True    
+    correct = 0
+    i = 0
+    while i < 1000:
+        try:
+            
+            correct += (checkresult(check_fall = fall))
+            i+=1
+            fall = not fall
+        except:
+            print(sys.exc_info()[0])
+    
+    print(correct)
 
